8e05149bb6cb907f7423fc79d5cc9bd6013ed4ef
Claudio Inglese <mrcode622@gmail.com>
2023-09-12 02:26:20 -0300

    PreEntrega1

--- js/main.js
+++ js/main.js
@@ -5,8 +5,7 @@ while (true){
         if (precio > 0) {
             console.log ("OK");
         }else{
-            alert("Ingresó un precio incorrecto. Vuelva a intentar")
-            console.log ("Ingresa un numero mayor a 0");
+            alert("Ingresó un precio incorrecto. Vuelva a intentar")            
             continue;
         }
         break;
@@ -17,19 +16,53 @@ while (true){
     }
 }
 
+let porcentajeIva
+while(true){
+    porcentajeIva = prompt("Ingrese 1 para IVA 21% o 2 para IVA 10,5%");
 
-function iva (precio){
+    if (porcentajeIva === '1'){
+        console.log ("Eligió valor 1");
+        let valorIva21 = iva21 (precio);
+        console.log("El precio del IVA es " + valorIva21);
+        iva21();
+        break;  
+        
+
+    }else if (porcentajeIva === '2'){
+        console.log ("Eligió valor 2");
+        let valorIva105 = iva105 (precio);
+        console.log("El precio del IVA es " + valorIva105);
+        iva105();   
+        break;
+        
+    }else{
+        alert("Opción no válida. Debe ingresar 1 o 2 para seleccionar el porcentaje de IVA.");
+        console.log ("Opción no válida. Debe ingresar 1 o 2 para seleccionar el porcentaje de IVA.");
+        
+    }
+}
+
+function iva21 (precio){
     return (precio * 0.21)
 }
 
-let valorIva = iva (precio)
-console.log("El precio del IVA es " + valorIva)
-
-
-function precioConIva (precio){
+function precioConIva21 (precio){
     return (precio  * 1.21)
 }
 
-let ivaIncluido = precioConIva(precio)
+function iva105 (precio){
+    return (precio * 0.105)
+}
 
-console.log("El precio IVA incluido es " + ivaIncluido)
+function precioConIva105 (precio){
+    return (precio  * 1.105)
+}
+
+let ivaIncluido 
+if ( porcentajeIva == 1){
+    ivaIncluido = precioConIva21(precio)
+}else{
+    ivaIncluido = precioConIva105(precio)
+}
+
+console.log("El precio IVA incluido es " + ivaIncluido);
